8e0ee647eb3423b8465dd7f085a0c20dbbf44977
phanthanhtan <thanhtan.021192@gmail.com>
2015-01-10 08:26:43 +0700

    nothing

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThanhThanhCong_test_web.MvcApplication" Language="C#" %>
---
+++ Models/TTC_HopDongThueDat.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'F:\DCT\windows form\ThanhThanhCong_test_web\ThanhThanhCong_test_web\Models\TTC_HopDongThueDat.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
